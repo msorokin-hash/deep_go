539667e5c907fdae7771a125bb5646b65b1efa7a
Михаил Сорокин <mihailsorokin@MacBook-Air-Mihail.local>
2025-08-07 14:56:23 +0300

    add scheduler

--- homework/goroutines_and_scheduler/homework_test.go
+++ homework/goroutines_and_scheduler/homework_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 
+	"container/heap"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,25 +14,82 @@ type Task struct {
 }
 
 type Scheduler struct {
-	// need to implement
+	priorityQueue *PriorityQueue
+	taskRegistry  map[int]*QueueItem
 }
 
 func NewScheduler() Scheduler {
-	// need to implement
-	return Scheduler{}
+	return Scheduler{
+		priorityQueue: &PriorityQueue{},
+		taskRegistry:  make(map[int]*QueueItem),
+	}
 }
 
 func (s *Scheduler) AddTask(task Task) {
-	// need to implement
+	if _, exists := s.taskRegistry[task.Identifier]; exists {
+		return
+	}
+
+	newItem := &QueueItem{
+		task:  &task,
+		index: -1,
+	}
+	s.taskRegistry[task.Identifier] = newItem
+	heap.Push(s.priorityQueue, newItem)
 }
 
 func (s *Scheduler) ChangeTaskPriority(taskID int, newPriority int) {
-	// need to implement
+	if item, exists := s.taskRegistry[taskID]; exists {
+		item.task.Priority = newPriority
+		heap.Fix(s.priorityQueue, item.index)
+	}
 }
 
 func (s *Scheduler) GetTask() Task {
-	// need to implement
-	return Task{}
+	if s.priorityQueue.Len() == 0 {
+		return Task{}
+	}
+
+	item := heap.Pop(s.priorityQueue).(*QueueItem)
+	delete(s.taskRegistry, item.task.Identifier)
+	return *item.task
+}
+
+type QueueItem struct {
+	task  *Task
+	index int
+}
+
+type PriorityQueue []*QueueItem
+
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].task.Priority > pq[j].task.Priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*QueueItem)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item
 }
 
 func TestTrace(t *testing.T) {
@@ -39,6 +98,7 @@ func TestTrace(t *testing.T) {
 	task3 := Task{Identifier: 3, Priority: 30}
 	task4 := Task{Identifier: 4, Priority: 40}
 	task5 := Task{Identifier: 5, Priority: 50}
+	task6 := Task{Identifier: 1, Priority: 100}
 
 	scheduler := NewScheduler()
 	scheduler.AddTask(task1)
@@ -56,7 +116,7 @@ func TestTrace(t *testing.T) {
 	scheduler.ChangeTaskPriority(1, 100)
 
 	task = scheduler.GetTask()
-	assert.Equal(t, task1, task)
+	assert.Equal(t, task6, task)
 
 	task = scheduler.GetTask()
 	assert.Equal(t, task3, task)
